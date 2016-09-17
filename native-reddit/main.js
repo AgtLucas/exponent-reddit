8e08408a34d1592a398136ed1924e9c9c46c9995
Lucas <lucas@agtlucas.com>
2016-09-17 06:57:30 -0300

    Standard js

--- native-reddit/main.js
+++ native-reddit/main.js
@@ -1,28 +1,28 @@
-import Exponent from 'exponent';
-import React from 'react';
+import Exponent from 'exponent'
+import React from 'react'
 import {
   StyleSheet,
   Text,
-  View,
-} from 'react-native';
+  View
+} from 'react-native'
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text>Open up main.js to start working on your app!</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
 
-Exponent.registerRootComponent(App);
+Exponent.registerRootComponent(App)
